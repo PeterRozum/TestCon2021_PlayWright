7757bb999a2176f3bdd522c763ebd34f9f894627
PeterRozum <43463808+PeterRozum@users.noreply.github.com>
2021-09-07 10:38:52 +0200

    first test

---
+++ tests/DuckDuckGoSmokeTest.spec.js
@@ -0,0 +1,7 @@
+const { test, expect } = require('@playwright/test');
+
+test('DuckDuckGo logo is visible', async ({ page }) => {
+    await page.goto('https://duckduckgo.com/');
+    const isLogoVisible = await page.isVisible('#logo_homepage_link');
+    await expect(isLogoVisible).toBe(true);
+  });
